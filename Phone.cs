a73dc435f2921f7437e354a6b65386ecc0aa5a8b
BUIHD1403 <huythong129@gmail.com>
2020-06-02 17:14:49 -0700

    Add files via upload

---
+++ Phone.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Phone : Form
+    {
+        SqlConnection con = new SqlConnection("ConnectionString");
+
+
+        public Phone()
+        {
+            InitializeComponent();
+        }
+
+        private void Phone_Load(object sender, EventArgs e)
+        {
+            this.ActiveControl = textBox4;
+            textBox4.Focus();
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Clear();
+            textBox3.Text = "";
+            textBox4.Clear();
+            comboBox1.SelectedIndex = -1;
+            textBox1.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand(@"INSERT INTO Mobiles +
+                                    (First, Last, Mobile, Category)
+                                    VALUES ('" + textBox1.Text + "','" + textBox2.Text + textBox3.Text + "','" + textBox4.Text + "','" + comboBox1.Text + "','", con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Sucessfully Save");
+            Display();
+        }
+
+        void Display()
+        {
+            SqlDataAdapter sda = new SqlDataAdapter("SELECT * from Mobiles",con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            dataGridView1.Rows.Clear();
+            foreach (DataRow item in dt.Rows)
+            {
+                int n = dataGridView1.Rows.Add();
+                dataGridView1.Rows[n].Cells[0].Value = item[0].ToString();
+                dataGridView1.Rows[n].Cells[1].Value = item[1].ToString();
+                dataGridView1.Rows[n].Cells[2].Value = item[2].ToString();
+                dataGridView1.Rows[n].Cells[3].Value = item[3].ToString();
+                dataGridView1.Rows[n].Cells[4].Value = item[4].ToString();
+
+            }
+        }
+    }
+}
